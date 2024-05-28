7b4f14ccb3251b4ce32814dba0f05c4999be700c
mphomolefe730 <mphomolefe730@gmail.com>
2024-05-28 17:52:09 +0200

    add the adding of product and getting of all products

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { environment } from './environment.js';
 import Express, { urlencoded } from 'express';
 import Mongoose from 'mongoose';
 import Cors from 'cors';
+import { productModel } from './productModel.js'
 
 // const environment = require('./environment.js');
 // const Express = require('express');
@@ -13,9 +14,35 @@ app.use(urlencoded({extended:true}));
 app.use(Cors({
     origin:'*',
 }));
-app.use('/',()=>{
-    console.log('test');
+
+app.get('/product',async (req,res)=>{
+    let listOfProduct = await productModel.find();
+    if (!listOfProduct) res.status(200).json({
+        message: "No products found",
+    })
+    else res.status(200).json({
+        message: "Success fetchinf products",
+        products: listOfProduct
+    })
 });
+
+app.post('/product/add', (req,res)=>{
+    const { productId, productName, productDescription, hash } = req.body;
+    let product = new productModel({
+        productId: productId, 
+        productName: productName, 
+        productDescription: productDescription, 
+        hash: hash
+    })
+    product.save()
+    .then(()=>res.send(product.json({
+        message: "Successfully added product",
+        productInfo: product
+    })))
+    .catch((err)=>res.status(400).json({
+        message: err,
+    }));
+})
 
 Mongoose.connect(`mongodb+srv://${environment.mongodb.username}:${environment.mongodb.password}@cluster0.mxwjh5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
